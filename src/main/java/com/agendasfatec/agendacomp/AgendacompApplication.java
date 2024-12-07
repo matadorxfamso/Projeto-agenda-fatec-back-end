9644f7581e4404d0912edafea84df6b0d175486d
MatheusPrazeres <matheusnathan320@gmail.com>
2024-12-07 01:42:01 -0300

    first commit

---
+++ src/main/java/com/agendasfatec/agendacomp/AgendacompApplication.java
@@ -0,0 +1,13 @@
+package com.agendasfatec.agendacomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendacompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendacompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendasfatec/agendacomp/AgendacompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendasfatec.agendacomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendacompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
